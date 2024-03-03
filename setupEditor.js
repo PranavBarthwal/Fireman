a2466e83190602def46ff6f362d1fe8e14abd076
PranavBarthwal <pbarthwal90@gmail.com>
2024-03-03 21:23:19 +0530

    fixed all issues and completed

--- setupEditor.js
+++ setupEditor.js
@@ -1,47 +1,44 @@
-import { EditorState, basicSetup } from "@codemirror/basic-setup";
-import { EditorView, keymap } from "@codemirror/view";
-import { defaultTabBinding } from '@codemirror/commands'
-import { json } from "@codemirror/lang-json";
+import { EditorState, basicSetup } from "@codemirror/basic-setup"
+import { defaultTabBinding } from "@codemirror/commands"
+import { EditorView, keymap } from "@codemirror/view"
+import { json } from "@codemirror/lang-json"
 
-export default function SetupEditors() {
-    const jsonRequestBody = document.querySelector("[data-json-request-body]");
-    const jsonResponseBody = document.querySelector("[data-json-response-body]");
+export default function setupEditors() {
+  const jsonRequestBody = document.querySelector("[data-json-request-body]")
+  const jsonResponseBody = document.querySelector("[data-json-response-body]")
 
+  const basicExtensions = [
+    basicSetup,
+    keymap.of([defaultTabBinding]),
+    json(),
+    EditorState.tabSize.of(2),
+  ]
 
-    const basicExtensions = [
-        basicSetup,
-        keymap.of([defaultTabBinding]),
-        json(),
-        EditorState.tabSize.of(2)
-    ];
+  const requestEditor = new EditorView({
+    state: EditorState.create({
+      doc: "{\n\t\n}",
+      extensions: basicExtensions,
+    }),
+    parent: jsonRequestBody,
+  })
 
-    const requestEditor = new EditorView({
-        state : EditorState.create({
-            doc: "{\n \t \n}",
-            extensions: basicExtensions
-        }),                               
-        parent : jsonRequestBody,
-    });
+  const responseEditor = new EditorView({
+    state: EditorState.create({
+      doc: "{}",
+      extensions: [...basicExtensions, EditorView.editable.of(false)],
+    }),
+    parent: jsonResponseBody,
+  })
 
-    const responseEditor = new EditorView({
-        state : EditorState.create({
-            doc: "{}",
-            extensions: [...basicExtensions, EditorView.editable.of(false)],
-        }),                               
-        parent : jsonResponseBody,
+  function updateResponseEditor(value) {
+    responseEditor.dispatch({
+      changes: {
+        from: 0,
+        to: responseEditor.state.doc.length,
+        insert: JSON.stringify(value, null, 2),
+      },
     })
+  }
 
-    function updateResponseEditor(value) {
-        responseEditor.dispatch({
-            changes: {
-                from : 0,
-                to : responseEditor.state.doc.length,
-                insert: JSON.stringify(value, null, 2)
-            }
-        })
-    }
-
-    return { requestEditor, updateResponseEditor };
+  return { requestEditor, updateResponseEditor }
 }
-
-
